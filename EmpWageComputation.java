3cfa2e4c5a17342f49730427d5768bc02bbacbee
301prateek <prateek.patil301@gmail.com>
2020-11-18 20:44:54 +0530

    Added: Calculating Wage For Month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,24 +2,35 @@
 
 public class EmpWageComputation {
 
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 
-		int empHrs  = 0;
+		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 
-		int empAttendance = (int) Math.floor(Math.random()*10)%3;
+		for (int i = 0; i < NUM_OF_WORKING_DAYS; i++) {
 
-		if(empAttendance == IS_FULL_TIME)
-			empHrs = 8;
-		else if (empAttendance == IS_PART_TIME)
-			empHrs = 4;
-		else
-			empHrs = 0;
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage : "+empWage);
+			int empAttendance = (int) Math.floor(Math.random() * 10)%3;
+
+			switch(empAttendance) {
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Employee Wage : "+empWage);
+		}
+		System.out.println("Total Employee Wage : "+totalEmpWage);
 	}
 }
