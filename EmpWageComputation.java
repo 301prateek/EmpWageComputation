e4bc3fb1ce30c1bec3737ab61852d9081314f1f8
301prateek <prateek.patil301@gmail.com>
2020-11-18 17:42:47 +0530

    Added: Check Employee Attendance

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,5 +4,13 @@ public class EmpWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+
+		int IS_FULL_TIME = 1;
+		int empAttendance = (int) Math.floor(Math.random()*10)%3;
+
+		if(empAttendance == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
