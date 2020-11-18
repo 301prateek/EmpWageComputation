9f9c3529a12abec8957e2dba3ff7242606384bb4
301prateek <prateek.patil301@gmail.com>
2020-11-18 22:50:05 +0530

    Added: Calculating Wage Till Total Working Hours or Max Days

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,19 +2,23 @@
 
 public class EmpWageComputation {
 
+
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
 
 		int empHrs = 0;
 		int empWage = 0;
-		int totalEmpWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
 
-		for (int i = 0; i < NUM_OF_WORKING_DAYS; i++) {
+		while( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 
+			totalWorkingDays++;
 			int empAttendance = (int) Math.floor(Math.random() * 10)%3;
 
 			switch(empAttendance) {
@@ -28,9 +32,10 @@ public class EmpWageComputation {
 					empHrs = 0;
 			}
 			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Employee Wage : "+empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day: "+totalWorkingDays + " Emp Hrs: "+empHrs);
 		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Employee Wage : "+totalEmpWage);
 	}
 }
